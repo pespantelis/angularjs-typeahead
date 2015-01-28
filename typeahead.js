aa2453f8ce094a3a11c4ceac08cbecb0a34fa411
Pantelis Peslis <pespantelis@gmail.com>
2015-01-28 23:51:23 +0200

    Initial commit.

---
+++ typeahead.js
@@ -0,0 +1,69 @@
+(function () {
+	'use strict';
+
+	angular.module('typeahead', [])
+
+	.directive('typeahead', ['$http', function ($http) {
+		return {
+			restrict: 'E',
+
+			scope: {
+				name: '@',
+				placeholder: '@',
+				params: '=',
+				queryKey: '@',
+				titleKey: '@',
+				subtitleKey: '@',
+			},
+
+			compile: function() {
+				return function(scope, elem, attrs) {
+
+					scope.update = function () {
+						// create the search query
+						var params = { params: { query: scope.model } };
+						$.each(scope.params, function(key) {
+							if(key !== scope.name && scope.params[key] !== null)
+								params.params[key] = scope.params[key].query;
+						});
+
+						// get data and highlight the first row
+						$http.get(attrs.src, params).then(function (response) {
+							scope.items = response.data;
+							scope.current = 0;
+						});
+
+						scope.params[scope.name] = null;
+						scope.hideList = false;
+					};
+
+					scope.handleSelection = function () {
+						var item = scope.items[scope.current];
+
+						if(item === null) {
+							scope.model = null;
+						}
+						else {
+							scope.model = item[scope.titleKey];
+							item.query = item[scope.queryKey];
+						}
+
+						scope.params[scope.name] = item;
+						scope.hideList = true;
+						scope.items = null;
+					};
+
+					scope.isActive = function (index) {
+						return scope.current == index;
+					};
+
+					scope.setActive = function (index) {
+						scope.current = index;
+					};
+				}
+			},
+
+			templateUrl: '/components/typeahead/typeahead.html'
+		};
+	}]);
+})();
